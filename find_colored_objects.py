08014571746f4b9e3a66daf41eb7a6d5ce8e022d
jekxkan <smirnova.eyu@mail.ru>
2024-10-01 20:46:21 +0300

    Ураааа

---
+++ find_colored_objects.py
@@ -0,0 +1,111 @@
+import numpy as np
+import cv2
+
+def find_red(image, image_hsv) -> int:
+    """
+    Устанавливает пороги для красного цвета, выделяет контуры красных объектов,
+    "упаковывает" в прямоугольник и подписывает
+
+    Args:
+        image: изображение с веб-камеры
+        image_hsv: изображение с веб-каемры в цветовом формате HSV
+
+    Returns:
+        count_red(int): количество красный объектов
+    """
+    # Установка порогов для красного цвета
+    lower_red = np.array([0, 100, 155])
+    upper_red = np.array([180, 255, 255])
+    red_mask = cv2.inRange(image_hsv, lower_red, upper_red)
+
+    # Выделение контуров, "упаковка" в прямоугольник и соответвутствующая подпись для красных объектов
+    kernel = np.ones((5, 5), np.uint8)
+    opening = cv2.morphologyEx(red_mask, cv2.MORPH_OPEN, kernel=kernel)
+
+    red_contours, _ = cv2.findContours(opening, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    red_contours = sorted(red_contours, key=cv2.contourArea, reverse=True)
+
+    for x in range(len(red_contours)):
+        area = cv2.contourArea(red_contours[x])
+        if area > 100:
+            x, y, width, height = cv2.boundingRect(red_contours[x])
+
+            image = cv2.rectangle(image, (x, y), (x + width, y + height), (255, 0, 0), 2)
+            image_red = cv2.rectangle(image, (x, y), (x + 60, y - 25), (0, 0, 0), -1)
+            cv2.putText(image_red, 'RED', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255), 2)
+    count_red = len(red_contours)
+
+    return count_red
+
+def find_green(image, image_hsv) -> int:
+    """
+    Устанавливает пороги для зеленого цвета, выделяет контуры зеленых объектов,
+    "упаковывает" в прямоугольник и подписывает
+
+    Args:
+        image: изображение с веб-камеры
+        image_hsv: изображение с веб-каемры в цветовом формате HSV
+
+    Returns:
+        count_green(int): количество зеленых объектов
+    """
+    # Установка порогов для зеленого цвета
+    lower_green = np.array([50, 60, 60])
+    upper_green = np.array([90, 255, 255])
+    green_mask = cv2.inRange(image_hsv, lower_green, upper_green)
+
+    # Выделение контуров, "упаковка" в прямоугольник и соответвутствующая подпись для зеленых объектов
+    kernel = np.ones((5, 5), np.uint8)
+    opening = cv2.morphologyEx(green_mask, cv2.MORPH_OPEN, kernel=kernel)
+
+    green_contours, _ = cv2.findContours(opening, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    green_contours = sorted(green_contours, key=cv2.contourArea, reverse=True)
+
+    for x in range(len(green_contours)):
+        area = cv2.contourArea(green_contours[x])
+        if area > 100:
+            x, y, width, height = cv2.boundingRect(green_contours[x])
+
+            image = cv2.rectangle(image, (x, y), (x + width, y + height), (255, 0, 0), 2)
+            image_red = cv2.rectangle(image, (x, y), (x + 100, y - 25), (0, 0, 0), -1)
+            cv2.putText(image_red, 'GREEN', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255), 2)
+    count_green = len(green_contours)
+
+    return count_green
+
+def find_blue(image, image_hsv) -> int:
+    """
+    Устанавливает пороги для синего цвета, выделяет контуры синих объектов,
+    "упаковывает" в прямоугольник и подписывает
+
+    Args:
+        image: изображение с веб-камеры
+        image_hsv: изображение с веб-каемры в цветовом формате HSV
+
+    Returns:
+        count_blue(int): количество синих объектов
+    """
+    # Установка порогов для синего цвета
+    lower_blue = np.array([90, 30, 40])
+    upper_blue = np.array([140, 255, 255])
+    blue_mask = cv2.inRange(image_hsv, lower_blue, upper_blue)
+
+    # Выделение контуров, "упаковка" в прямоугольник и соответвутствующая подпись для синих объектов
+    kernel = np.ones((5, 5), np.uint8)
+    opening = cv2.morphologyEx(blue_mask, cv2.MORPH_OPEN, kernel=kernel)
+
+    blue_contours, _ = cv2.findContours(opening, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    blue_contours = sorted(blue_contours, key=cv2.contourArea, reverse=True)
+
+    for x in range(len(blue_contours)):
+        area = cv2.contourArea(blue_contours[x])
+        if area > 100:
+            x, y, width, height = cv2.boundingRect(blue_contours[x])
+
+            image = cv2.rectangle(image, (x, y), (x + width, y + height), (255, 0, 0), 2)
+            image_red = cv2.rectangle(image, (x, y), (x + 80, y - 25), (0, 0, 0), -1)
+            cv2.putText(image_red, 'BLUE', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255), 2)
+    count_blue = len(blue_contours)
+
+    return count_blue
+
--- main.py
+++ main.py
@@ -1,89 +1,35 @@
 import cv2
-import numpy as np
-
-def nothing(x):
-    pass
+from find_colored_objects import find_red, find_blue, find_green
 
 capture= cv2.VideoCapture(0)
-# cv2.namedWindow('frame')
-# cv2.createTrackbar('H', 'frame', 0,180, nothing)
-# cv2.createTrackbar('S', 'frame', 0,255, nothing)
-# cv2.createTrackbar('V', 'frame', 0,255, nothing)
-#
-# cv2.createTrackbar('HL', 'frame', 0,180, nothing)
-# cv2.createTrackbar('SL', 'frame', 0,255, nothing)
-# cv2.createTrackbar('VL', 'frame', 0,255, nothing)
 
 while True:
     return_code, image = capture.read()
-    image_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     blurred_image = cv2.GaussianBlur(image, (3, 3), 0)
+    image_hsv = cv2.cvtColor(blurred_image, cv2.COLOR_BGR2HSV)
 
-    # h = cv2.getTrackbarPos('H', 'frame')
-    # s = cv2.getTrackbarPos('S', 'frame')
-    # v = cv2.getTrackbarPos('V', 'frame')
+    # #Объединение масок всех цветов
+    # combined_mask = red_mask | get_green_mask(image_hsv) | get_blue_mask(image_hsv)
     #
-    # hl = cv2.getTrackbarPos('HL', 'frame')
-    # sl = cv2.getTrackbarPos('SL', 'frame')
-    # vl = cv2.getTrackbarPos('VL', 'frame')
-    #
-    # lower = np.array([hl, sl, vl])
-    # upper = np.array([h, s, v])
-    # mask = cv2.inRange(image_hsv, lower, upper)
+    # # Применение маски для всех цветов к изображению
+    # result = cv2.bitwise_and(image, image, mask=mask)
 
-    # Установка порогов для синего цвета
-    lower_blue = np.array([110, 30, 115])
-    upper_blue = np.array([130, 255, 255])
-    blue_mask = cv2.inRange(image_hsv, lower_blue, upper_blue)
+    find_red(image, image_hsv)
+    find_green(image, image_hsv)
+    find_blue(image, image_hsv)
 
-    # Установка порогов для зеленого цвета
-    lower_green = np.array([50, 60, 60])
-    upper_green = np.array([90, 255, 255])
-    green_mask = cv2.inRange(image_hsv, lower_green, upper_green)
-
-    # Установка порогов для красного цвета
-    lower_red = np.array([0, 100, 155])
-    upper_red = np.array([60, 255, 255])
-    red_mask = cv2.inRange(image_hsv, lower_red, upper_red)
-
-    # Объединение масок всех цветов
-    combined_mask = red_mask | blue_mask | green_mask
-
-    # Применение маски к заблюренному изображению
-    result = cv2.bitwise_and(image, image, mask=combined_mask)
-    kernel = np.ones((5, 5), np.uint8)
-    opening = cv2.morphologyEx(combined_mask, cv2.MORPH_OPEN, kernel=kernel)
-    closing = cv2.morphologyEx(opening, cv2.MORPH_OPEN, kernel=kernel)
-
-    contours, _ = cv2.findContours(closing, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-    contours = sorted(contours, key = cv2.contourArea, reverse = True)
-
-    for x in range(len(contours)):
-        area = cv2.contourArea(contours[x])
-        if area > 100:
-            x, y, width, height = cv2.boundingRect(contours[x])
-            image_blue = cv2.rectangle(image, (x, y), (x + width, y + height), (255, 0, 0), 2)
-            image_blue = cv2.rectangle(image_blue, (x, y), (x + 60, y - 25), (0, 0 ,0), -1)
-            cv2.putText(image_blue, 'BLUE', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1.0,(255, 255, 255), 2)
-
-            image_red = cv2.rectangle(image, (x, y), (x + width, y + height), (255, 0, 0), 2)
-            image_red = cv2.rectangle(image_red, (x, y), (x + 60, y - 25), (0, 0, 0), -1)
-            cv2.putText(image, 'RED', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255), 2)
-
-            image_green = cv2.rectangle(image, (x, y), (x + width, y + height), (255, 0, 0), 2)
-            image_green = cv2.rectangle(image_green, (x, y), (x + 60, y - 25), (0, 0, 0), -1)
-            #cv2.putText(image_green, 'GREEN', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255), 2)
-
-
-    cv2.imshow('opening', image)
-    cv2.imshow('result', closing)
-
+    cv2.imshow('From camera', image)
+    # cv2.imshow('detection', result)
 
     #Код нажатой клавиши, побитовая операция гарантирует, что код будет соотвествовать ASCII-кодам символов
     key = cv2.waitKey(30) & 0xFF
     #27 номер соответствует клавише "Escape"
     if key == 27:
         break
+
+print(f"Количество красных объектов на последнем кадре: {find_red(image, image_hsv)}")
+print(f"Количество зеленых объектов на последнем кадре: {find_green(image, image_hsv)}")
+print(f"Количество синих объектов на последнем кадре: {find_blue(image, image_hsv)}")
 
 cv2.destroyAllWindows()
 
